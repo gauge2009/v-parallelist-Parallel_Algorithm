baa9751f740533a1b8a4c4e175501ce70b307e7d
gauger2009 <gauge2009@vip.sina.com>
2022-02-10 20:44:29 +0800

    v-parallelist-(7.3)- 多线程实现归并排序 22.2.10 v-parallelist-(7.4)- CAS算法-Compare-And-Swap 22.2.10

--- src/parallel/AlgorithmPSO.java
+++ src/parallel/AlgorithmPSO.java
@@ -1,103 +1,89 @@
 package parallel;
 
-
+import java.util.concurrent.atomic.AtomicInteger;
 
 //函数y=-x*(x-2) 在[0,2]上最大值的粒子群算法
-
-
 public class AlgorithmPSO {
-	int n=2;//粒子数量
-	double []y;
-	double []x;
-	double []v;
-	double c1=2;
-	double c2=2;
+	int n = 2;//粒子数量
+	double[] y;
+	double[] x;
+	double[] v;
+	double c1 = 2;
+	double c2 = 2;
 	double pbest[];
 	double gbest;
-	double vmax=0.1;//速度最大值
-	
+	double vmax = 0.1;//速度最大值
+
 	//适应度函数--  并发执行加快速度
 	public void fitnessFN() {
-		for(int i=0;i<n;i++) {
-			y[i]=-1*x[i]*(x[i]-2);
-			
-
+		for (int i = 0; i < n; i++) {
+			y[i] = -1 * x[i] * (x[i] - 2);
 		}
-
 	}
-	
+
 	//初始化
 	public void init() {
-		this.x=new double[n];
-		this.y=new double[n];
-		this.v=new double[n];
-		this.pbest=new double[n];
-		
-		x[0]=0.0;
-		x[1]=2.0;
-		v[0]=0.01;
-		v[1]=0.02;
+		this.x = new double[n];
+		this.y = new double[n];
+		this.v = new double[n];
+		this.pbest = new double[n];
+
+		x[0] = 0.0;
+		x[1] = 2.0;
+		v[0] = 0.01;
+		v[1] = 0.02;
 		fitnessFN();
 		//初始化，寻找最优
-		for(int i=0;i<n;i++) {
-			pbest[i]=y[i];//赋值
-			if(y[i]>gbest) {
-				gbest=y[i];//保存最大
+		for (int i = 0; i < n; i++) {
+			pbest[i] = y[i];//赋值
+			if (y[i] > gbest) {
+				gbest = y[i];//保存最大
 			}
 		}
-		System.out.println("原始最优解"+gbest);
-		
-		
+		System.out.println("原始最优解" + gbest);
 	}
+	private AtomicInteger count = new AtomicInteger(0);//统计迭代次数
 	//粒子群算法
 	public void PSO(int max) {
-		for(int i=0;i<max;i++) {
-			
-		
-			new Thread(()->{
-				double w=0.4;//全局搜索
-				for(int j=0;j<n;j++) {
-					
+		for (int i = 0; i < max; i++) {
+			new Thread(() -> {
+				double w = 0.4;//全局搜索
+				for (int j = 0; j < n; j++) {
 					//更新速度位置
-					v[j]=w*v[j]+c1*Math.random()*(pbest[j]-x[j])+c2*Math.random()*(gbest-x[j]);
-					if(v[j]>vmax) {
-						v[j]=vmax;//控制速度不超过最大
+					v[j] = w * v[j] + c1 * Math.random() * (pbest[j] - x[j]) + c2 * Math.random() * (gbest - x[j]);
+					if (v[j] > vmax) {
+						v[j] = vmax;//控制速度不超过最大
 					}
-					x[j]+=v[j];//更新位置
-					
-					if(x[j]>2) {
-						x[j]=2;
+					x[j] += v[j];//更新位置
+
+					if (x[j] > 2) {
+						x[j] = 2;
 					}
-					if(x[j]<0) {
-						x[j]=0;
+					if (x[j] < 0) {
+						x[j] = 0;
 					}
-					
 				}
 				fitnessFN();
 				//更新个体极值与群体极值
-				for(int j=0;j<n;j++) {
-					pbest[j]=Math.max(y[j], pbest[j]);//取得最大
-					if(pbest[j]>gbest) {
-						gbest=pbest[j];//保存群体最大值
+				for (int j = 0; j < n; j++) {
+					pbest[j] = Math.max(y[j], pbest[j]);//取得最大
+					if (pbest[j] > gbest) {
+						gbest = pbest[j];//保存群体最大值
 					}
-					System.out.println("粒子n"+j+" :x:="+x[j]+" v="+v[j]);
+					System.out.println("粒子n" + j + " :x:=" + x[j] + " v=" + v[j]);
 				}
-				System.out.println("第"+"K次迭代，全局最优gbest= "+gbest);
+				int K = this.count.incrementAndGet();
+				System.out.println("第" + K +"次迭代，全局最优gbest= " + gbest);
 				System.out.println();
-				
-				
-			}) .start();
-			
+			}).start();
 		}
 	}
-	
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 AlgorithmPSO  ap=new  AlgorithmPSO();
-		 ap.init();
-		 ap.PSO(100);
+		AlgorithmPSO ap = new AlgorithmPSO();
+		ap.init();
+		ap.PSO(100);
 	}
-
 }
 
--- src/parallel/Merge.java
+++ src/parallel/Merge.java
@@ -5,42 +5,35 @@ import java.util.concurrent.*;
 
 //1928    4756
 //19  28   47  56
-//1 9   28   47  56
+//1 9   2 8   4 7  5 6
 //1289     4567
 //12456789
 
-
 public class Merge {
-
-	
-	public  static  class MergeSort extends RecursiveAction{
-		private int []arr;
-		public   MergeSort( int []arr) {
-			this.arr=arr;
+	public static class MergeSort extends RecursiveAction {
+		private int[] arr;
+		public MergeSort(int[] arr) {
+			this.arr = arr;
 		}
 		@Override
 		protected void compute() {
 			// TODO Auto-generated method stub
-			if(this.arr.length>1) {
-				int []leftarr=Arrays.copyOfRange(arr, 0,arr.length/2);
-				int []rightarr=Arrays.copyOfRange(arr,arr.length/2, arr.length);
-				
-				invokeAll(new MergeSort(leftarr),new MergeSort(rightarr));//初始化
-				merge(leftarr,rightarr,arr);//合并排序
+			if (this.arr.length > 1) {
+				int[] leftarr = Arrays.copyOfRange(arr, 0, arr.length / 2);
+				int[] rightarr = Arrays.copyOfRange(arr, arr.length / 2, arr.length);
+
+				invokeAll(new MergeSort(leftarr), new MergeSort(rightarr));//初始化
+				merge(leftarr, rightarr, arr);//合并排序
 			}
-			
-			
-			
 		}
 		/**
 		 * 合并排序
-		 * 
+		 *
 		 * @param leftArray
 		 * @param rightArray
 		 * @param intArr
 		 */
 		private void merge(int[] leftArray, int[] rightArray, int[] intArr) {
-
 			// i：leftArray数组索引，j：rightArray数组索引，k：intArr数组索引
 			int i = 0, j = 0, k = 0;
 			while (i < leftArray.length && j < rightArray.length) {
@@ -54,7 +47,6 @@ public class Merge {
 				}
 				k++;
 			}
-
 			// 将还剩余元素没有遍历完的数组直接追加到intArr后面
 			if (i == leftArray.length) {
 				for (; j < rightArray.length; j++, k++) {
@@ -66,24 +58,14 @@ public class Merge {
 				}
 			}
 		}
-
-		
 	}
-	
-	
-	
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[]arr= {1, 3, 2, 8, 4, 6, 7, 5, 10, 9, 12, 11};
-	    ForkJoinPool pool=new ForkJoinPool();
-	    pool.invoke(new MergeSort(arr) );//fork  join线程池
-	    for(int i=0;i<arr.length;i++) {
-	    	System.out.println(arr[i]);
-	    }
-	    
-	    
-	    
+		int[] arr = {1, 3, 2, 8, 4, 6, 7, 5, 10, 9, 12, 11};
+		ForkJoinPool pool = new ForkJoinPool();
+		pool.invoke(new MergeSort(arr));//fork  join线程池
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 	}
-
 }
